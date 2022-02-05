35af33b18481de78d626e35ddee699677c3c51bc
Bhagwat Karankar <dev.pacific22@gmail.com>
2022-02-05 12:52:18 +0530

    Added hamburger menu and its handling.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Contact from "./components/contact/Contact";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
@@ -7,15 +8,19 @@ import Work from "./components/work/Work";
 import "./app.scss";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <div className="app">
-      <Topbar />
+      <Topbar menuOpen={menuOpen} toggleMenu={toggleMenu} />
       <div className="sections">
-        <Intro/>
-        <Portfolio/>
-        <Work/>
-        <Testimonials/>
-        <Contact/>
+        <Intro />
+        <Portfolio />
+        <Work />
+        <Testimonials />
+        <Contact />
       </div>
     </div>
   );
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,25 +1,29 @@
 import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, toggleMenu }) {
   return (
-    <div className="topbar" id="topbar">
+    <div className={"topbar " + (menuOpen && "active")} id="topbar">
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className="logo">
             Pacific
           </a>
           <div className="itemContainer">
-            <Person className="icon"/>
+            <Person className="icon" />
             <span>+91 7709588XXX</span>
           </div>
           <div className="itemContainer">
-            <Mail className="icon"/>
+            <Mail className="icon" />
             <span>dev.pacific22@gmail.com</span>
           </div>
         </div>
         <div className="right">
-          <h2> Menu </h2>
+          <div className="hamburger" onClick={() => toggleMenu()}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
     </div>
