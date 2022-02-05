d74ea7e4cd2fa2bf7d7d7e7e2592bd0fd7ed4191
Bhagwat Karankar <dev.pacific22@gmail.com>
2022-02-05 19:44:16 +0530

    Added intro section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,7 +1,38 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
-  return <div className='intro' id="intro">
-      
-  </div>;
+  const textRef = useRef();
+  useEffect(() => {
+    console.log(textRef);
+    init(textRef.current, {
+      showCursor: false,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["React Native", "Android", "iOS", "Flutter"],
+    });
+  }, []);
+
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src={"assets/man.png"} alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Bhagwat Karankar</h1>
+          <h3>
+            Mobile app developer with <span ref={textRef} />
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  );
 }
