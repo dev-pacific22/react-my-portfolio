67285668fae2b76d300e7ab76ed6e6c03007fceb
Bhagwat Karankar <dev.pacific22@gmail.com>
2022-02-08 16:13:00 +0530

    Added portfolio basic skeleton.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,71 @@
-import "./portfolio.scss"
+import { useState } from "react";
+import PortfolioList from "../portfoliolist/PortfolioList";
+import "./portfolio.scss";
 
 export default function Portfolio() {
-  return <div className='portfolio' id="portfolio"></div>;
+  const [selected, setSelected] = useState("featured");
+  const listFolio = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "react-native",
+      title: "React Native",
+    },
+    {
+      id: "android",
+      title: "Android",
+    },
+    {
+      id: "ios",
+      title: "iOS",
+    },
+    {
+      id: "flutter",
+      title: "Flutter",
+    },
+  ];
+
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {listFolio.map((item) => (
+          <PortfolioList
+            id={item.id}
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/content/soccer.jpg" alt="" />
+          <h3>GDPR Consent App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/content/soccer.jpg" alt="" />
+          <h3>GDPR Consent App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/content/soccer.jpg" alt="" />
+          <h3>GDPR Consent App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/content/soccer.jpg" alt="" />
+          <h3>GDPR Consent App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/content/soccer.jpg" alt="" />
+          <h3>GDPR Consent App</h3>
+        </div>
+        <div className="item">
+          <img src="assets/content/soccer.jpg" alt="" />
+          <h3>GDPR Consent App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
